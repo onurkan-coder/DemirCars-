028d59ecec224660b9962b953a988df1d1720440
onurkan-coder <onurkandemir037@gmail.com>
2025-04-26 18:46:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Sayfa içeriği tamamen yüklendiğinde çalışacak fonksiyonu tanımlar
+document.addEventListener("DOMContentLoaded", () => {
+    // Sayfa yüklendiğinde konsola mesaj yazdırır
+    console.log("Bootstrap destekli Gezi Rehberi yüklendi!");
+});
